70ea6f3c8d21cc96fbf9770cb9c712b994555683
duykhang-droid <duykhangok@gmail.com>
2026-05-19 22:56:55 +0000

    g

--- src/main.cpp
+++ src/main.cpp
@@ -439,7 +439,7 @@ void setup() {
     tft.setRotation(bruceConfigPins.rotation);
     tft.fillScreen(TFT_BLACK);
     // bruceConfig is not read yet.. just to show something on screen due to long boot time
-    tft.setTextColor(TFT_PURPLE, TFT_BLACK);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
     tft.drawCentreString("Pe Giau", tft.width() / 2, tft.height() / 2, 1);
 #else
     tft.begin();
