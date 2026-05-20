df66680ff8e9aa0c90f9ef2e1ef3a3c36f60f077
duykhang-droid <duykhangok@gmail.com>
2026-05-20 10:41:59 +0000

    ggggghh

--- src/core/utils.h
+++ src/core/utils.h
@@ -6,7 +6,6 @@ void backToMenu();
 void addOptionToMainMenu();
 int getBattery() __attribute__((weak));
 void updateClockTimezone();
-void updateTimeStr(struct tm timeInfo);
 void showDeviceInfo();
 String formatTimeDecimal(uint32_t totalMillis);
 String getOptionsJSON();
