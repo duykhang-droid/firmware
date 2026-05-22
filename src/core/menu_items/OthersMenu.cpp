1e53ef379967a3430a79bc12a051b04f673afaf5
duykhang-droid <duykhangok@gmail.com>
2026-05-22 06:16:59 +0000

    ggvvgvvvv

--- src/core/menu_items/OthersMenu.cpp
+++ src/core/menu_items/OthersMenu.cpp
@@ -5,21 +5,13 @@
 #include "modules/badusb_ble/ducky_typer.h"
 #include "modules/bjs_interpreter/interpreter.h"
 #include "modules/others/clicker.h"
-#include "modules/others/ibutton.h"
-#include "modules/others/mic.h"
-#include "modules/others/qrcode_menu.h"
 #include "modules/others/tururururu.h"
 #include "modules/others/u2f.h"
 // Removed: #include "modules/others/timer.h"
 
 void OthersMenu::optionsMenu() {
     options = {
-        {"QRCodes",      qrcode_menu                  },
         {"Megalodon",    shark_setup                  },
-
-#if defined(MIC_SPM1423) || defined(MIC_INMP441)
-        {"Microphone",   [this]() { micMenu(); }      }, //@deveclipse
-#endif
 
 // New consolidated BadUSB & HID submenu
 #if !defined(LITE_VERSION)
@@ -27,13 +19,6 @@ void OthersMenu::optionsMenu() {
         {"BadUSB & HID", [this]() { badUsbHidMenu(); }},
 #endif
 #endif
-
-#ifndef LITE_VERSION
-        {"iButton",      setup_ibutton                },
-#endif
-
-        // Timer removed - moved to another "Clock"
-    };
 
     addOptionToMainMenu();
     loopOptions(options, MENU_TYPE_SUBMENU, "Others");
@@ -55,18 +40,6 @@ void OthersMenu::badUsbHidMenu() {
     };
 
     loopOptions(options, MENU_TYPE_SUBMENU, "BadUSB & HID");
-}
-
-void OthersMenu::micMenu() {
-    options = {
-#if defined(MIC_SPM1423) || defined(MIC_INMP441)
-        {"Spectrum", mic_test                   },
-        {"Record",   mic_record_app             },
-#endif
-        {"Back",     [this]() { optionsMenu(); }},
-    };
-
-    loopOptions(options, MENU_TYPE_SUBMENU, "Microphone");
 }
 
 void OthersMenu::drawIcon(float scale) {
