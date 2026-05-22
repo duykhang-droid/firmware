a07ca451b6e8a493446393cb712712cc86d93b60
duykhang-droid <duykhangok@gmail.com>
2026-05-22 11:18:42 +0000

    xoatio

--- src/main.cpp
+++ src/main.cpp
@@ -136,7 +136,6 @@ volatile int tftHeight = VECTOR_DISPLAY_DEFAULT_WIDTH;
 #include "core/wifi/webInterface.h"
 #include "core/wifi/wifi_common.h"
 #include "modules/bjs_interpreter/interpreter.h" // for JavaScript interpreter
-#include "modules/others/audio.h"                // for playAudioFile
 #include "modules/rf/rf_utils.h"                 // for initCC1101once
 #include <Wire.h>
 
