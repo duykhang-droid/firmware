752f824b8504c56ad45c8d4bbd89feaeb6a906c2
duykhang-droid <duykhangok@gmail.com>
2026-06-29 11:59:33 +0000

    sửa màn

--- lib/TFT_eSPI/User_Setups/Setup43_ST7735.h
+++ lib/TFT_eSPI/User_Setups/Setup43_ST7735.h
@@ -18,12 +18,12 @@
 //  #define TFT_RGB_ORDER TFT_BGR  // Colour order Blue-Green-Red
 
 #ifdef ESP32
-#define TFT_MISO 19
-#define TFT_MOSI 23
+#define TFT_MISO -1
+#define TFT_MOSI 19
 #define TFT_SCLK 18
-#define TFT_CS    15  // Chip select control pin
-#define TFT_DC    2  // Data Command control pin
-#define TFT_RST   4  // Reset pin (could connect to RST pin)
+#define TFT_CS    5  // Chip select control pin
+#define TFT_DC    16  // Data Command control pin
+#define TFT_RST   23  // Reset pin (could connect to RST pin)
 //#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
 #else
 // Display GND       to NodeMCU pin GND (0V)
--- src/core/display.cpp
+++ src/core/display.cpp
@@ -12,7 +12,7 @@
 
 // Send the ST7789 into or out of sleep mode
 void panelSleep(bool on) {
-#if defined(ST7789_2_DRIVER) || defined(ST7789_DRIVER)
+#if defined(ST7735_2_DRIVER) || defined(ST7735_DRIVER)
     if (on) {
         tft.writecommand(0x10); // SLPIN: panel off
         delay(5);
